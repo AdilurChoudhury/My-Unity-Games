3221e441ed30647abbea976eb8174386c124959c
Adilur Choudhury <38773062+AdilurChoudhury@users.noreply.github.com>
2022-02-14 12:50:40 -0500

    Added Fake Platforms
    Add fake platforms that spawn after a couple normal platforms spawn. Be on the lookout for them.

--- Scavenger Hunt/AC-SketchHead/Assets/Scripts/GameController.cs
+++ Scavenger Hunt/AC-SketchHead/Assets/Scripts/GameController.cs
@@ -8,15 +8,26 @@ public class GameController : MonoBehaviour
     [Header("Platform Object")]
     public GameObject platform;
 
+    [Header("Fake Platform Object")]
+    public GameObject fakePlatform;
+
     [Header("Game Over Canvas Object")]
     public GameObject gameOverCanvas;
     //Default position for platform
     float pos = 0;
+    float fakePos = 5;
     void Start()
     {
-        for (int i = 0; i < 1000; i++) {
-            SpawnPlatforms();
+        if (this.name == GameObject.Find("GameController").name) {
+            for (int i = 0; i < 1000; i++) {
+                SpawnPlatforms();
+            }
+        } else {
+            for (int i = 0; i < 500; i++) {
+                SpawnFakePlatforms();
+            }
         }
+            
     }
 
     // Update is called once per frame
@@ -25,13 +36,19 @@ public class GameController : MonoBehaviour
 
     }
 
+    void SpawnFakePlatforms() {
+        Instantiate(fakePlatform, new Vector3(Random.value * 12 - 5f, fakePos, 0.5f), Quaternion.identity);
+        fakePos += 8.0f;
+    }
     void SpawnPlatforms() {
         //Spawn platforms randomly on the x axis and spawn them on the y axis every 2.5
         Instantiate(platform, new Vector3(Random.value * 10 - 5f, pos, 0.5f), Quaternion.identity);
-        pos += 2.5f;
+        pos += 4.0f;
     }
 
     public void GameOver() {
         gameOverCanvas.SetActive(true);
     }
+
+    
 }
