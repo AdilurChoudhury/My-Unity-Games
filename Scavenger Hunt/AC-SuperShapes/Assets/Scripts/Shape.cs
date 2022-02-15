1ba99521f67323df3d7b8a2c6072fb574e80bb8c
Adilur Choudhury <38773062+AdilurChoudhury@users.noreply.github.com>
2022-02-14 23:40:19 -0500

    Added Super Shapes Game
    Added super shapes game.
    
    In this game, you play as a circle that needs to rotate out of the way of incoming shapes. Each shape has an opening that the player can navigate through. You also gain points for each shape you navigate through.

---
+++ Scavenger Hunt/AC-SuperShapes/Assets/Scripts/Shape.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shape : MonoBehaviour
+{
+    [Header("RigidBody Object")]
+    public Rigidbody2D rb;
+    //Shrinking speed
+    [Header("Default Shrinking Speed")]
+    public float shrinkSpeed = 3f;
+
+    void Start()
+    {
+        //Rotation of rigidbody at random range
+        rb.rotation = Random.Range(0f, 360f);
+        //Local scale for hexagon
+        transform.localScale = Vector3.one * 10f;
+
+
+    }
+
+    void Update()
+    {
+        //Polygon shrink rate
+        transform.localScale -= Vector3.one * shrinkSpeed * Time.deltaTime;
+        //Destroy if too small
+        if (transform.localScale.x <= .05f) {
+            Destroy(gameObject);
+            Score.score++;
+        }
+    }
+}
