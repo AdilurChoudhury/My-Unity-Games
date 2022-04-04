3521d9df176ba54d9a743fcfa99bb34bf16fb0ac
Adilur Choudhury <38773062+AdilurChoudhury@users.noreply.github.com>
2022-04-04 04:30:48 -0400

    Added Best Score Text
    Best Score is recorded between games.

--- AC-DroppingBombs2/Assets/Scripts/GameManager.cs
+++ AC-DroppingBombs2/Assets/Scripts/GameManager.cs
@@ -16,19 +16,28 @@ public class GameManager : MonoBehaviour
     public Text scoreText;
     public int pointsWorth = 1;
     private int score;
+    public Text bestScoreText;
+    private int bestScore;
+    private bool beatBestScore;
+    private bool isPlayerAlive = true;
     private bool smokeCleared = true;
+
     void Awake() 
     {
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         player = playerPrefab;
         scoreText.enabled = false;
+        bestScoreText.enabled = false;
     }
     void Start()
     {
         spawner.active = false;
         title.SetActive(true);
         splash.SetActive(false);
+
+        bestScore = PlayerPrefs.GetInt("BestScore");
+        bestScoreText.text = "Best Score: " + bestScore.ToString();
     }
 
     // Update is called once per frame
@@ -43,7 +52,7 @@ public class GameManager : MonoBehaviour
             }
         } else 
         {
-            if (!player)
+            if (!isPlayerAlive)
             {
                 OnPlayerKilled();
             }
@@ -65,6 +74,17 @@ public class GameManager : MonoBehaviour
                 Destroy(bombObject);
             }
         }
+
+        if (!gameStarted) {
+            var textColor = "#323232";
+            
+            if (beatBestScore) {
+                textColor = "#F00";
+            }
+            bestScoreText.text = "<color=" + textColor + ">Best Score: " + bestScore.ToString() + "</color>";
+        } else {
+            bestScoreText.text = "";
+        }
     }
 
     void ResetGame()
@@ -78,6 +98,9 @@ public class GameManager : MonoBehaviour
         scoreText.enabled = true;
         scoreSystem.GetComponent<Score>().score = 0;
         scoreSystem.GetComponent<Score>().Start();
+        
+        beatBestScore = false;
+        bestScoreText.enabled = true;
     }
 
     void OnPlayerKilled() 
@@ -85,6 +108,15 @@ public class GameManager : MonoBehaviour
         spawner.active = false;
         gameStarted = false;
         Invoke("SplashScreen", 2f);
+
+        score = scoreSystem.GetComponent<Score>().score;
+
+        if (score > bestScore) {
+            bestScore = score;
+            PlayerPrefs.SetInt("BestScore", bestScore);
+            beatBestScore = true;
+            bestScoreText.text = "Best Score: " + bestScore.ToString();
+        }
     }
 
     void SplashScreen() 
--- AC-DroppingBombs2/Assets/Scripts/TriggerExplosion.cs
+++ AC-DroppingBombs2/Assets/Scripts/TriggerExplosion.cs
@@ -6,9 +6,10 @@ public class TriggerExplosion : MonoBehaviour
 {
     [Header("Explosion Parts")]
     public GameObject explosion;
-
     private void OnCollisionEnter(Collision collision) 
     {
         Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+        
     }
 }
