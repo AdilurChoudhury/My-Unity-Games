a5a4d422195275557d243c53fc406e6d4f128614
Adilur Choudhury <38773062+AdilurChoudhury@users.noreply.github.com>
2022-04-10 07:58:37 -0400

    Added JungleEscapePY
    Created Jungle Escape Prove yourself using raycasting to detecting platforms with no colliders.

---
+++ JungleEscapePY-AC/Assets/Scripts/DetectFalsePlatforms.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectFalsePlatforms : MonoBehaviour
+{
+    public bool hit;
+    void Update()
+    {
+        hit = Physics.Raycast(transform.position, Vector3.forward, 3, 1 << 8);
+        Debug.DrawRay(transform.position, transform.forward * 3, Color.red);
+        if (hit) {
+            Debug.Log("All Clear!");
+        } else {
+            Debug.LogWarning("Be Careful!");
+        }
+    }
+}
